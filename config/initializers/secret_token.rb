b855c33e5c0333e06c735a3e38051f9d3b314fa1
artemb1393 <darkallenb1394@gmail.com>
2012-11-13 21:50:42 +0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qwerty::Application.config.secret_token = '08c53e925ef048ff28d6ae8b27b06d63cc1176865880845097e8e50eaaa3c225431d80e3e0ddb52508722ae61cf5eb54b5c8c9662c1b6a63696df0f2cb944c7d'
